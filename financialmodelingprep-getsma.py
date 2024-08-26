825541f388ace00476fee4007bb701aade9434ac
shlomo koren (momo) <shlomo.koren@gmail.com>
2024-08-26 15:26:55 +0300

    dev micha 150 rang

--- financialmodelingprep-getsma.py
+++ financialmodelingprep-getsma.py
@@ -5,13 +5,14 @@ def isNeedSell(data):
     result = False
     closedValue = int(data[0]["close"])
     smaValue = int(data[0]["sma"])
-    if (smaValue >=closedValue ):
+#    smaValueBefore = int(data[7]["sma"])
+    if (smaValue >=closedValue ) :
         result = True
         return result
     return (result)
 def getsma(symbol,smarange,apikey):
     sma=0
-    url = "https://financialmodelingprep.com/api/v3/technical_indicator/1day/AAPL?type=sma&period=150&apikey=XOdeeszqGm4RohYI1hZH1dJb92ALCFZN"
+#    url = "https://financialmodelingprep.com/api/v3/technical_indicator/1day/AAPL?type=sma&period=150&apikey=XOdeeszqGm4RohYI1hZH1dJb92ALCFZN"
     payload = {}
     headers = {}
     params = {
@@ -24,15 +25,43 @@ def getsma(symbol,smarange,apikey):
 #    print(response.status_code)
     data = response.json()
     isSell = isNeedSell(data)
+    msg = "test "+symbol +", rang " + str(smarange) +",sma "+ str(int(data[0]["sma"])) +",close "+ str(int(data[0]["close"]))
     if isSell:
-        print(symbol + ", rang " + str(smarange) + ",sma " + str(int(data[0]["sma"])) + ",close " + str(
-            int(data[0]["close"]))+ ", You have to sell")
+        print(msg + ", You have to sell")
+        sendtegrammsg(msg  + ", You have to sell")
     else:
-        print(symbol +", rang " + str(smarange) +",sma "+ str(int(data[0]["sma"])) +",close "+ str(int(data[0]["close"])) )
+        print(msg)
+def sendtegrammsg(message):
+    # Replace 'your_bot_token' with your bot's token
+    bot_token = os.getenv("bot_token")
+
+    # Replace 'your_chat_id' with the chat ID or the recipient's user ID
+    chat_id = '420134022'
+
+    # The message you want to send
+
+    # Telegram API URL for sending messages
+    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+
+    # Parameters for the API request
+    params = {
+        'chat_id': chat_id,
+        'text': message
+    }
+
+    # Send the message
+    response = requests.get(url, params=params)
+
+    # Check if the message was sent successfully
+    if response.status_code == 200:
+        print("Message sent successfully!")
+    else:
+        print("Failed to send message.")
+
 
 apikey = os.getenv("apikey")
 symbols = [{"symbol":'AAPL',"range":150},
-           {"symbol":'AFRM',"range":300},
+           {"symbol":'AFRM',"range":200},
            {"symbol":'AMD',"range":300},
            {"symbol":'CRM',"range":300},
            {"symbol":'GOOG',"range":150},
