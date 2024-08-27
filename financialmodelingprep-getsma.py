f1d1dbcfeed68b851b6b84ba77f3783b5dd1cace
Koren shlomo <shlomo.koren01@outlook.com>
2024-08-27 16:20:55 +0300

    add buy alert support

--- financialmodelingprep-getsma.py
+++ financialmodelingprep-getsma.py
@@ -1,19 +1,33 @@
+import json
 import requests
 import os
 from datetime import datetime
+
+def isNeedBuy(data):
+    result = False
+    closedValue = int(data[0]["close"])
+    smaValue = int(data[0]["sma"])
+#    smaValueBefore = int(data[7]["sma"])
+    if (smaValue < closedValue ) :
+        result = True
+        return result
+    return (result)
+
 
 def isNeedSell(data):
     result = False
     closedValue = int(data[0]["close"])
     smaValue = int(data[0]["sma"])
-#    smaValueBefore = int(data[7]["sma"])
+    smaValueBefore = int(data[7]["sma"])
     if (smaValue >=closedValue ) :
         result = True
         return result
     return (result)
 def getsma(symbol,smarange,action,apikey):
     sma=0
-#    url = "https://financialmodelingprep.com/api/v3/technical_indicator/1day/AAPL?type=sma&period=150&apikey=XOdeeszqGm4RohYI1hZH1dJb92ALCFZN"
+    logfile_path = os.getenv("logfile_path")
+
+    #    url = "https://financialmodelingprep.com/api/v3/technical_indicator/1day/AAPL?type=sma&period=150&apikey=XOdeeszqGm4RohYI1hZH1dJb92ALCFZN"
     payload = {}
     headers = {}
     params = {
@@ -25,10 +39,10 @@ def getsma(symbol,smarange,action,apikey):
     response = requests.request("GET", url, headers=headers, data=payload,params=params)
 #    print(response.status_code)
     data = response.json()
+    msg = symbol + ', action ' + action + ", rang " + str(smarange) + ",sma " + str(
+        int(data[0]["sma"])) + ",close " + str(int(data[0]["close"]))
     if  action == "sell":
         isSell = isNeedSell(data)
-        msg = symbol +', action '+action +", rang " + str(smarange) +",sma "+ str(int(data[0]["sma"])) +",close "+ str(int(data[0]["close"]))
-        logfile_path = "E:\\momotools\\getsellaction\\logfile.txt"
         if isSell:
             print(msg + ", You have to sell")
             sendtegrammsg(msg  + ", You have to sell")
@@ -36,6 +50,18 @@ def getsma(symbol,smarange,action,apikey):
         else:
             print(msg)
             writeToLogfile(msg , logfile_path)
+    elif action == "buy":
+        isBuy = isNeedBuy(data)
+        if isBuy:
+            print(msg + ", You have to buy")
+            sendtegrammsg(msg  + ", You have to buy")
+            writeToLogfile(msg + ", You have to buy", logfile_path)
+        else:
+            print(msg)
+            writeToLogfile(msg , logfile_path)
+
+
+
 def sendtegrammsg(message):
     # Replace 'your_bot_token' with your bot's token
     bot_token = os.getenv("bot_token")
@@ -67,24 +93,21 @@ def writeToLogfile(line,logfile_path):
     now = datetime.now()
     # Format it as a string
     current_time = now.strftime("%Y-%m-%d %H:%M:%S")
-    logfile_path = "E:\\momotools\\getsellaction\\logfile.txt"
+    logfile_path = os.getenv("logfile_path")
     # Open the file in append mode ('a')
     with open(logfile_path, "a") as logfile:
         # Write a line to the log file
         logfile.write(current_time +","+ line +" \n")
 
+debug = True
 apikey = os.getenv("apikey")
-symbols = [{"symbol":'AAPL',"range":150,"action": 'sell'},
-           {"symbol":'AFRM',"range":300,"action": 'sell'},
-           {"symbol":'AMD',"range":300,"action": 'sell'},
-           {"symbol":'CRM',"range":300,"action": 'sell'},
-           {"symbol":'GOOG',"range":150,"action": 'sell'},
-           {"symbol": 'MSFT', "range": 200,"action": 'sell'},
-           {"symbol": 'MSI', "range": 150,"action": 'sell'},
-           {"symbol": 'NVDA', "range": 150,"action": 'sell'},
-           {"symbol": 'ORCL', "range": 150,"action": 'sell'},
-           {"symbol": 'ASML', "range": 200,"action": 'sell'},
-           {"symbol": 'ARKW', "range": 150,"action": 'sell'},
-           {"symbol": 'AMZN', "range": 200,"action": 'sell'}]
+# Read the JSON file
+with open('data_invest.json', 'r') as file:
+    symbols = json.load(file)
 for item in symbols:
-    getsma(item["symbol"],item["range"],item["action"],apikey)
+    if debug == True:
+     if item["symbol"] == "AMZN":
+        getsma(item["symbol"],item["range"],item["action"],apikey)
+    else:
+        getsma(item["symbol"],item["range"],item["action"],apikey)
+
