e0cdbeccac2fd03c0dead731641bdd9de313cc97
Koren shlomo <shlomo.koren01@outlook.com>
2024-09-21 18:47:19 +0300

    publish version 0.0.5beta

--- invest_ma150_alert.py
+++ invest_ma150_alert.py
@@ -14,7 +14,7 @@ from datetime import datetime, timedelta
 from pandas.core.computation.common import result_type_many
 
 debug = False
-__version__ = "0.0.4beta"
+__version__ = "0.0.5beta"
 
 
 def get_general_parameters():
